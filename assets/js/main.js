1c3e025959507c7c41892ef37be046d5a75b9ceb
Eric Gardner <gardner.ec@gmail.com>
2015-01-21 12:20:05 -0800

    Add support for smooth-state js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ library: jquery-2.1.3.min.js
 ---
 // If using a JS framework, include it here
 {% include_relative _lib/{{page.library}} %}
+{% include_relative _lib/jquery.smoothState.js %}
 
 // Application code goes here
 $(document).ready(function(){
@@ -19,6 +20,28 @@ $(document).ready(function(){
     e.preventDefault();
   });
 });
+
+// smoothstate JS
+;(function($) {
+  'use strict';
+  var $body = $('html, body'),
+      content = $('#main').smoothState({
+        // Runs when a link has been activated
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function (url, $container) {
+            // toggleAnimationClass() is a public method
+            // for restarting css animations with a class
+            content.toggleAnimationClass('is-exiting');
+            // Scroll user to the top
+            $body.animate({
+              scrollTop: 0
+            });
+          }
+        }
+      }).data('smoothState');
+      //.data('smoothState') makes public methods available
+})(jQuery);
 
 
 // Vanilla JS equivalent of $(document).ready
