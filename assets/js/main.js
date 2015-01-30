1b8b400e133f968fc61793da56fa700c2cd2bcb0
Eric Gardner <gardner.ec@gmail.com>
2015-01-29 16:15:47 -0800

    WIP: Attempting to get lunr search working

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,32 @@ library: jquery-2.1.3.min.js
 {% include_relative _lib/{{page.library}} %}
 {% include_relative _lib/jquery.smoothState.js %}
 {% include_relative _lib/list.js %}
+{% include_relative _lib/lunr.min.js %}
 
 // Initialization scripts
 {% include_relative smoothstate_init.js %}
-{% include_relative setUpTable.js %}
+{% include_relative set_up_table.js %}
+
+
+// $.getJSON( "{{site.baseurl}}/test.json", function( data ) {
+//   var items = [];
+//   $.each( data, function( key, val ) {
+//     items.push( "<li id='" + key + "'>" + val + "</li>" );
+//   });
+//   console.log("JSON loaded");
+ 
+  // $( "<ul/>", {
+  //   "class": "my-new-list",
+  //   html: items.join( "" )
+  // }).appendTo( "search-results" );
+// });
+
+var req = $.getJSON("{{site.baseurl}}/test.json")
+.done(
+  function() {
+    window.json = req.responseJSON;
+  };
+);
 
 // Application scripts
 $(document).ready(function(){
@@ -26,15 +48,5 @@ $(document).ready(function(){
     });
   });
 
-  
   setUpTable();
-
 });
-
-  // test code: needs to go inside document.ready
-  // $('.table-controls a').on('click touchstart', function(e){
-  //   target = $(this).data('sort');
-  //   e.preventDefault;
-  //   flag = true;
-  //   window.pubsSurvey.sort(target, {insensitive: true, desc: flag});
-  // });
