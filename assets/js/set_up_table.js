ae2da72ca11b3693698d2a23ff01554d2cfc56a5
Eric Gardner <gardner.ec@gmail.com>
2015-02-04 15:31:52 -0800

    Table sort updates

--- assets/js/set_up_table.js
+++ assets/js/set_up_table.js
@@ -4,4 +4,10 @@ function setUpTable(){
     valueNames: [ 'title', 'institution', 'type', 'format', 'price' ]
   };
   var surveyTable = new List('pubs_survey', options);
+
+  $("#pubs_survey").on("keyup", "input", function () {
+    var numResults = $("#pubs_survey tbody tr").length;
+    $(".results").text(numResults);
+  });
 }
+
